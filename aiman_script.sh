ca13c0505c81af9c658845cc9602ec474a3ab140
Aiman Zafar <aniqaz456@gmail.com>
2024-12-29 00:11:34 +0500

    new script added

--- aiman_script.sh
+++ aiman_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo " here are my os info "
-OS=$(cat /etc/os-release )
+OS=$(cat /etc/os-release | grep "^NAME="  )
 if [[ "$OS" == *"Linux"* ]]; then
     echo "You are running Linux. Nice choice! ($OS)"
 elif [[ "$OS" == *"Darwin"* ]]; then
